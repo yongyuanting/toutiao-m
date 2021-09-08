8d1c119ef60fe77eb4eabc88fed9d65fda369605
yyt <1207046999@qq.com>
2021-09-08 14:22:18 +0800

    项目初始化-调整项目结构

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './style/index.less'
+// 加载全局样式
 
 Vue.config.productionTip = false
 
