fd727b43a2d0b858b397827e0e10b240ec06e849
yyt <1207046999@qq.com>
2021-09-08 15:11:27 +0800

    引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <!--    路由出口-->
     <router-view/>
     <h1>头条</h1>
+    <div>
+      <i class="iconfont icon-dianzan2"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './style/index.less'
 // 加载全局样式
+import './style/index.less'
+
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
+// 注册使用vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
